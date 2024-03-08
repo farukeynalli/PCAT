202026e24e8fed93da4f4982adcaf28bd0301dc3
farukeynalli <afarukeynalli@gmail.com>
2024-03-08 16:09:50 +0300

    public file created

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-function yazdir() {
-    console.log('hello');
-}
+const express = require('express');
+const path = require('path');
+const app = express();
 
-yazdir();
+app.use(express.static('public'));
 
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda çalışmaya başlamıştır.. `);
+});

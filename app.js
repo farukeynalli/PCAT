494bd9edcb1cdf5b65221e6b288fe81b86af8ebc
farukeynalli <afarukeynalli@gmail.com>
2024-03-08 17:05:39 +0300

    template engine has been created

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
+
 const app = express();
 
+// template engine ejs
+app.set('view engine', 'ejs');
+
+// middleware
 app.use(express.static('public'));
 
+// routes
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
 });
 
 app.listen(port, () => {
